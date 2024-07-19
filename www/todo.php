0216c56d52508ed3a9d3c62f64901f75e9499100
victorchoque <choque.chambilla.victor.2022@incoslapaz.org>
2024-07-18 22:59:03 -0400

    Librerias Agregadas para validaciones

--- www/todo.php
+++ www/todo.php
@@ -5,5 +5,12 @@ require __DIR__. '/modelos/Producto.class.php';
 require __DIR__. '/modelos/Proveedor.class.php';
 //require __DIR__. '/librerias/tuadmin/funciones.php';
 require __DIR__. '/librerias/tuadmin/CRUD.php';
+require __DIR__. '/vendor/autoload.php';
+//validar json con schema
+/* //eejemplo
+use Swaggest\JsonSchema\Schema;
+$schema = Schema::import(json_decode($schemaJson));
+$schema->in($array_del_json); //  Exception: cuando algo no esta bien
+*/
 $url = sprintf('mysql://%s:%s@%s/%s', DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_NAME);
 DB::init($url);
